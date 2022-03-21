2c6d1c391208daadb201b699b4ef43a550cc6b2d
Ibrahim Ajibosin <82901442+Ibrahim-Ajibosin@users.noreply.github.com>
2022-03-21 10:40:00 +0100

    Initial commit

---
+++ Number_conversion.py
@@ -0,0 +1,39 @@
+import math
+
+def con(n):
+   
+   x,y = 0,0
+   yr , m ,wk  = 365 , 30 , 7 
+   
+   if n > yr :
+    num_of_years = math.floor(n/yr)
+    x = n%yr 
+   else:
+      if n < yr :
+        num_of_years = 0
+        x = n
+      else:
+         num_of_years = 1
+   if x > m :
+    num_of_months = math.floor(x/m)
+    y = x%m
+   else:
+      if x < m :
+          num_of_months = 0
+          y = x
+      else:
+         num_of_months =1 
+
+   if y > wk:
+     num_of_wks = math.floor(y/wk)
+     num_of_days = y%wk
+   else:
+      if y < wk :
+          num_of_wks = 0
+          num_of_days = y
+      else:
+         num_of_wks =1 
+
+   print(num_of_years,  num_of_months , num_of_wks, num_of_days)    
+
+con(1000)
